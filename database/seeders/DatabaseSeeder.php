1b6c7a9f38d6fa2e836619352cc4387e7d76cac8
Kevin Ceferino <kevin.ceferino000@gmail.com>
2022-04-17 19:10:04 -0500

    A

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        App\Models\Project::create([
+            'title' => 'Iglesia Vertical Barranquilla',
+            'url' => 'iglesia_vertical_barranquilla',
+            'description' => 'Iglesia vertical barranquilla es un proyecto creado plenamente en HTML5,CSS3,JS, PHP, Bootstrap'
+        ]);
+        App\Models\Project::create([
+            'title' => 'Intranet Montechelo',
+            'url' => 'intranet_montechelo',
+            'description' => 'Intranet Montechelo es una plataforma hecha para la empresa Montechelo, blah blah'
+        ]);
+
+        App\Models\User::create([
+            'name' => 'Kevin Ceferino',
+            'email' => 'kevin@test.com',
+            'password' => bcrypt('159753')
+        ]);
     }
 }
